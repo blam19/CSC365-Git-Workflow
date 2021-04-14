60a999fd5d92a7dcf79c2f7d629c0e47f6b89277
kenneth <kyleandken@yahoo.com>
2021-04-14 01:07:55 -0700

    yes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("4 years at Cal Poly");
 		System.out.println("Brandon Lam");
+		System.out.println("Kenneth modify good");
 	}
 }
